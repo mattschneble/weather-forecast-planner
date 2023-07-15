4c3efcdcd0b95b79a3690114336aa03cf9d4e768
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-07-14 20:00:49 -0700

    added functions to search for a city and retrieve weather conditions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //declare needed global variables
 var cities = [];
 var apiKey = "a92822d61b314348f615f16c6e001a99";
-var today = daysj().format("MM/DD/YYYY");
+var todayDate = daysj().format("MM/DD/YYYY");
 var localCity;
 //declare DOM variables
 var localWeatherContainer = document.querySelector("#local-weather-container");
@@ -11,3 +11,49 @@ var cityInput = document.querySelector("#city-input");
 var searchButton = document.querySelector("#search-button");
 var fiveDayForecastContainer = document.querySelector("#five-day-forecast-container");
 var fiveDayForecast = document.querySelector("#five-day-forecast");
+
+//create function to search for a given city
+function searchCity(city) {
+    //create the URL to search for the city
+    var searchCityURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "{state code},{country code}&limit=3&appid=" + apiKey;
+//use fetch to get the data from the API
+    fetch(searchCityURL)
+    //convert a successful response to JSON
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(weatherConditions) {
+                //use the returned latitude and longitude to get weather conditions
+                retrieveWeatherConditions(weatherConditions[0].lat, weatherConditions[0].lon);
+            });
+            //if the response is not ok, alert the user
+        } else {
+            alert('Unable to retrieve weather conditions for ' + city);
+        }
+        //if the fetch fails, alert the user
+    }).catch(function(error) {
+        alert('Unable to retrieve weather conditions for ' + city + "due to bad API connection. Please try again later.");
+    });
+}
+
+//create function to retrieve weather conditions using the latitude and longitude
+function retrieveWeatherConditions(lat, lon) {
+    //create the URL to get the weather conditions
+    var retrieveWeatherConditionsURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
+    //use fetch to get the data from the API
+    fetch(retrieveWeatherConditionsURL)
+    //convert a successful response to JSON
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(weatherConditions) {
+                //use the returned data to display the weather conditions
+                displayWeatherConditions(weatherConditions);
+            });
+            //if the response is not ok, alert the user
+        } else {
+            alert('Unable to retrieve weather conditions for ' + city);
+        }
+        //if the fetch fails, alert the user
+    }).catch(function(error) {
+        alert('Unable to retrieve weather conditions for ' + city + "due to bad API connection. Please try again later.");
+    });
+}
